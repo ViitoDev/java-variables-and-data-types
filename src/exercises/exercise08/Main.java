e51e76cb2e50f5ec549d81f51578383c6a18ce54
João Barros <joaovictorpiresdev@gmail.com>
2026-06-25 09:50:01 -0300

    feat: adding the exercise 8

---
+++ src/exercises/exercise08/Main.java
@@ -0,0 +1,10 @@
+package exercises.exercise08;
+
+public class Main {
+    public static void main(String[] args) {
+        double valueReais = 50;
+        double dollarValue = 5.22;
+
+        System.out.println("R$" + valueReais + " is equals US$" + (valueReais / dollarValue));
+    }
+}
