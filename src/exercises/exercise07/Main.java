0186d2f99985d8dc0196f9f867e74971fcd1cabc
João Barros <joaovictorpiresdev@gmail.com>
2026-06-25 09:40:37 -0300

    feat: Adding the exercise 7

---
+++ src/exercises/exercise07/Main.java
@@ -0,0 +1,13 @@
+package exercises.exercise07;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 2;
+
+        if (number % 2 == 0) {
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+    }
+}
